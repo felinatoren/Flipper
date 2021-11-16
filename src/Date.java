d4c13392adaecb99f83aa3eb2343cd26b90c6ebd
tobias juul Rasmussen <tobiasras.job@gmail.com>
2021-11-16 21:51:48 +0100

    Kan nu checkke om en bruger er over 18 år ud fra dato

---
+++ src/Date.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
+
+public class Date {
+
+    LocalDate today = LocalDate.now();
+
+    public boolean isOver18(String birthday){
+        Scanner scanner = new Scanner(birthday);
+        scanner.useDelimiter("/");
+
+        int day = scanner.nextInt();
+        int month = scanner.nextInt();
+        int year = scanner.nextInt();
+
+        LocalDate membersBirthday = LocalDate.of(year,month,day);
+
+        long daysBetween = ChronoUnit.DAYS.between(membersBirthday, today);
+
+        Boolean isOver18;
+
+        // You are 18, if you are 6574 days old.
+        if (daysBetween > 6574){
+            isOver18 = true;
+        } else
+            isOver18 = false;
+
+        return isOver18;
+    }
+}
