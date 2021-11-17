5f81264d7acff7d002a1fd3839099486fb3c623c
mikkel <mikk017y@stud.kea.dk>
2021-11-17 10:13:51 +0100

    med Member og Elite klasser

---
+++ src/Elite.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Elite extends Member {
+
+    private ArrayList<Elite> SwimType = new ArrayList<>();
+
+    public void addMember(SwimType) {
+        elite.add(Member);
+
+
+    }
+}
---
+++ src/Member.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+public class Member {
+
+    boolean active = true;
+    String birthday = "0";
+    String navn = "0";
+    int restance = 0;
+    int memberID = 0;
+
+    ArrayList<String> elite = new ArrayList<String>();
+}
+
+    public ArrayList []{
+        }
